8b26f69e5361a00ee6c57d030bf513605a20e65b
jihwan2da <wlghks8982@naver.com>
2023-09-20 20:33:02 +0900

    ExceptionTest

---
+++ src/test/kotlin/com/test/kotlin/KotlinExceptionTest.kt
@@ -0,0 +1,15 @@
+package com.test.kotlin
+
+import io.kotlintest.specs.StringSpec
+
+class KotlinExceptionTest : StringSpec() {
+
+    /**
+     * try-catch-finally 구문은 java와 다를게 없다.
+     * 다른점은 이 또한 Expession으로 동작하는 점이다.
+     *
+     *
+     * Kotlin에서는 CheckedException과 UnCheckedException을 구분하지 않는다.
+     * 모두 UnCheckedException으로 간주한다.
+     */
+}
