665521cb03bae78528d8d2e1b079b85a54336095
jihwan2da <wlghks8982@naver.com>
2023-09-25 23:51:14 +0900

    AbstractClassTest

---
+++ src/test/kotlin/com/test/kotlin/AbstractClassTest.kt
@@ -0,0 +1,49 @@
+package com.test.kotlin
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class AbstractClassTest : StringSpec(){
+
+    init {
+        "abstract class" {
+            val cat = Cat(species = "고양이과")
+            val penguin = Penguin(species = "펭귄과")
+
+            cat.legCount shouldBe 4
+            penguin.wingCount shouldBe 2
+            penguin.legCount shouldBe 4
+        }
+    }
+}
+
+abstract class Animal(
+    val species: String,
+    open val legCount: Int
+) {
+    abstract fun move()
+}
+
+class Cat(
+    species: String
+) : Animal(species, 4) {
+
+    override fun move() {
+        println("고양이 걸음")
+    }
+}
+
+class Penguin(
+    species: String,
+//    private val wingCount: Int = 2
+) : Animal(species, 2) {
+
+    val wingCount: Int = 2
+
+    override fun move() {
+        println("펭귄이 걸음")
+    }
+
+    override val legCount: Int
+        get() = super.legCount + this.wingCount
+}
